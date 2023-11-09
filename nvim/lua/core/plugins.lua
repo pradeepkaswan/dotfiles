32a3a5c143862b60831a2acc95db4998bd71fbb2
Pradeep Kaswan <76137397+pradeepkaswan@users.noreply.github.com>
2023-11-09 13:52:57 +0530

    Add files via upload

---
+++ nvim/lua/core/plugins.lua
@@ -0,0 +1,66 @@
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+    use 'wbthomason/packer.nvim'
+
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.4',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
+
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+
+    use('github/copilot.vim')
+
+    use({
+        'rose-pine/neovim',
+        as = 'rose-pine',
+        config = function()
+            vim.cmd [[colorscheme rose-pine]]
+        end
+    })
+
+    use('tpope/vim-fugitive')
+
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        requires = {
+            --- Uncomment these if you want to manage LSP servers from neovim f
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
+
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'L3MON4D3/LuaSnip' },
+        }
+    }
+
+    --fullstack dev
+    use('pangloss/vim-javascript')    --JS support
+    use 'leafgarland/typescript-vim' --TS support
+    use 'maxmellon/vim-jsx-pretty'   --JS and JSX syntax
+    use 'jparise/vim-graphql'        --GraphQL syntax
+    use 'mattn/emmet-vim'
+
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if packer_bootstrap then
+        require('packer').sync()
+    end
+end)
---
+++ nvim/lua/core/set.lua
@@ -0,0 +1,29 @@
+vim.opt.guicursor = "n-v-c-i:block"
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.mouse = "a"
+vim.opt.fileencoding = "utf-8"
+vim.opt.pumheight = 10
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.cursorline = true
+vim.opt.conceallevel = 0
+vim.opt.updatetime = 50
